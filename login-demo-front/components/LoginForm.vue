37a2ac736d61d2b2f44990be7ce60873698d48e8
RyoHirano <68998057+RyoHirano@users.noreply.github.com>
2021-05-01 14:48:21 +0900

    feat:front login. (#1)
    Co-authored-by: Ryo Hirano <ryo.hirano@monocrea.co.jp>

---
+++ login-demo-front/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-title>login</v-card-title>
+    <v-card-text>
+      <v-text-field label="id" prepend-icon="mdi-account" />
+      <v-text-field label="password" type="password" prepend-icon="mdi-lock" />
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer />
+      <v-btn id="login" color="primary" class="px-5" @click="login">
+        login
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class LoginForm extends Vue {
+  login() {
+    console.log('hoge')
+  }
+}
+</script>
